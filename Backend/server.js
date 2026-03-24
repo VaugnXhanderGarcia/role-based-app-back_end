ecdb20d2fea875463690b40c98c86d94ac65e15c
Vaugn Xhander A. Garcia <xhander.garcia@gmail.com>
2026-03-24 17:31:48 +0800

    Corrected Cors pathways

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,8 @@ const PORT = 3000;
 const SECRET_KEY = 'your-very-secure-secret'; 
 
 app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'] 
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500','http://127.0.0.1:5501', 
+    'http://localhost:5501'] 
 }));
 
 app.use(express.json());
